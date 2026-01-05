b6497e02daf5d8d9722b0a2d3a0be04562d029b0
Nikita Orlenko <nikita.orlenko234@gmail.com>
2026-01-05 13:42:02 +0200

    Ma tegin uuendamine Lisa funktsioon ja natue ma saan lisada punktid

--- funktsioonid.php
+++ funktsioonid.php
@@ -15,7 +15,7 @@ function lisapunkt($id){
 function naitaTabel(){
 
     global $yhendus;
-    $paring = $yhendus->prepare("Select id, presedent, pilt, punktid, lisamisaeg, kommentaarid from valimised where avalik=1");
+    $paring = $yhendus->prepare("Select id, presedent, pilt, punktid, lisamisaeg, kommentaarid from valimised where avalik=1 or avalik=0");
     $paring->bind_result($id, $presedent, $pilt, $punktid, $lisamisaeg, $kommentaarid);
     $paring->execute();
     while ($paring->fetch()) {
@@ -29,14 +29,14 @@ function naitaTabel(){
 
 
 //uue presidenti lisamine INSERT
-function lisaPresident()
+function lisaPresident($presedent, $pilt, $punktid)
 {
     global $yhendus;
     $paring = $yhendus->prepare("
-        INSERT INTO valimised (presedent, pilt, lisamisaeg)
-        VALUES (?, ?, NOW())"
+        INSERT INTO valimised (presedent, pilt, punktid, lisamisaeg)
+        VALUES (?, ?,?, NOW())"
     );
-    $paring->bind_param('ss', $presedent, $pilt);
+    $paring->bind_param('ssi', $presedent, $pilt, $punktid );
     $paring->execute();
     $yhendus->close();
 }
--- valimisedFunktsioonidega.php
+++ valimisedFunktsioonidega.php
@@ -6,6 +6,13 @@ if (isset($_REQUEST['lisa1punkt'])){
     header("Location: " . $_SERVER['PHP_SELF']);
     exit();
 }
+//paring lisaPresident funktsiooni otsimiseks
+if (!empty($_REQUEST['presedentNimi'])) {
+    lisaPresident($_REQUEST['presedentNimi'], $_REQUEST['pilt'], $_REQUEST['punktid']);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -33,6 +40,22 @@ if (isset($_REQUEST['lisa1punkt'])){
     //funktsioon mis näitab tabeli asub funktsioonid.php failis
     naitaTabel();
     ?>
+</table>
+
+<h2>Lisa oma presidenti</h2>
+<form action="">
+    <label for="presedentNimi">President nimi:</label>
+    <input type="text" name="presedentNimi">
+    <br>
+    <label for="pilt">Presidenti pilt</label>
+    <textarea name="pilt" id="pilt"></textarea>
+    <br>
+    <label for="punktid">Presedenti punktid</label>
+    <textarea name="punktid" id="punktid"></textarea>
+    <br>
+    <input type="submit" value="Lisa">
+</form>
+
 
 </body>
 </html>
